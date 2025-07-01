a3b85f24d17b1247257eed3c99cd55d13870ef9e
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-07-01 18:38:55 +0530

    Solved the assignments

--- node-app/index.js
+++ node-app/index.js
@@ -2,3 +2,32 @@
 let x = 1;
 x = "Bhumesh";
 console.log(x);
+// Problem 1: Write a function that greets a user given their first name. 
+//Argument - firstName
+//Logs - Hello {firstName}
+//Doesn’t return anything
+function greetUser(fName) {
+    console.log(`Hello ${fName}`);
+}
+greetUser("Bhumesh");
+// Problem 2: Write a function that calculates the sum of two Numbers
+function sumNum(a, b) {
+    return a + b;
+}
+console.log(sumNum(5, 99));
+// Problem 3 - Return true or false based on if a user is 18+
+function isLegal(age) {
+    if (age >= 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isLegal(50));
+console.log(isLegal(14));
+// Problem 4: Create a function that takes another function as input, and runs it after 1 second.
+function runAfterOneSec(func) {
+    setTimeout(func, 1000);
+}
+runAfterOneSec(() => console.log("harkirat"));
--- node-app/index.ts
+++ node-app/index.ts
@@ -1,3 +1,37 @@
 let x: number|string = 1;
 x = "Bhumesh";
 console.log(x);
+// Problem 1: Write a function that greets a user given their first name. 
+//Argument - firstName
+//Logs - Hello {firstName}
+//Doesn’t return anything
+function greetUser(fName: string){
+    console.log(`Hello ${fName}`)
+
+}
+greetUser("Bhumesh");
+
+// Problem 2: Write a function that calculates the sum of two Numbers
+function sumNum(a: number, b: number): number{
+    return a + b;
+}
+console.log(sumNum(5,99));
+
+// Problem 3 - Return true or false based on if a user is 18+
+function isLegal(age: number): boolean{
+    if(age>=18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isLegal(50));
+console.log(isLegal(14));
+
+// Problem 4: Create a function that takes another function as input, and runs it after 1 second.
+
+function runAfterOneSec(func: ()=>void){
+    setTimeout(func, 1000);
+}
+runAfterOneSec(()=> console.log("harkirat"));
